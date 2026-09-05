1ffc2aca1324affc7fcd696942ead3a4e1f3ad78
deepakydv25 <deepaksyadav00@gmail.com>
2026-09-05 20:17:14 +0530

    cointrail-api: initial commit

---
+++ cointrail-api/src/main/java/com/deepak/cointrailapi/CointrailApiApplication.java
@@ -0,0 +1,13 @@
+package com.deepak.cointrailapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CointrailApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CointrailApiApplication.class, args);
+    }
+
+}
---
+++ cointrail-api/src/test/java/com/deepak/cointrailapi/CointrailApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepak.cointrailapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CointrailApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
