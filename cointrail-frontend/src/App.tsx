7f33d76ff5b670f4e9e54f7a9e96df020b354c05
deepakydv25 <deepaksyadav00@gmail.com>
2026-09-15 12:27:50 +0530

    added protected router

--- cointrail-frontend/src/App.tsx
+++ cointrail-frontend/src/App.tsx
@@ -4,6 +4,7 @@ import LoginPage from "./pages/LoginPage"
 import RegisterPage from "./pages/RegisterPage"
 import ExpensesPage from "./pages/ExpensesPage"
 import Navbar from "./components/Navbar"
+import ProctectedRoute from "./routes/ProtectedRouter"
 
 function App() {
 
@@ -12,11 +13,14 @@ function App() {
       <Navbar />
       
       <Routes>
-        <Route path="/" element={<DashboardPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/dashboard" element={<DashboardPage />} />
-        <Route path="/expenses" element={<ExpensesPage />} />
+
+        <Route element={<ProctectedRoute />}>
+          <Route path="/" element={<DashboardPage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/expenses" element={<ExpensesPage />} />
+        </Route>
       </Routes>
     </>
   );
